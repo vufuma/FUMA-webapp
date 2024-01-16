b206c2c0b9d0eca37532a07ac2857c0c84b695c1
Anastasios Mitsigkolas <tasos1109@gmail.com>
2024-01-16 10:31:09 +0100

    parameters and GRCh38

--- scripts/gwas_file/examples/gwas_file_new.py
+++ scripts/gwas_file/examples/gwas_file_new.py
@@ -29,10 +29,28 @@ param = configparser.RawConfigParser()
 param.optionxform = str
 param.read(filedir / 'params.config') # read the params.config file
 
-##### check format of pre-defined lead SNPS and genomic regions if files are provided #####
+##### prepare parameters #####
 leadfile = param.get('inputfiles', 'leadSNPsfile')
 regionfile = param.get('inputfiles', 'regionsfile')
 
+gwas = filedir / cfg.get('inputfiles', 'gwas')
+outSNPs = filedir / "input.snps"
+outMAGMA = filedir / "magma.in"
+
+chrcol = param.get('inputfiles', 'chrcol').upper()
+poscol = param.get('inputfiles', 'poscol').upper()
+rsIDcol = param.get('inputfiles', 'rsIDcol').upper()
+pcol = param.get('inputfiles', 'pcol').upper()
+neacol = param.get('inputfiles', 'neacol').upper()
+eacol = param.get('inputfiles', 'eacol').upper()
+orcol = param.get('inputfiles', 'orcol').upper()
+becol = param.get('inputfiles', 'becol').upper()
+secol = param.get('inputfiles', 'secol').upper()
+Ncol = param.get('params', 'Ncol').upper()
+GRCh38 = param.get('params', 'GRCh38')
+N = param.get('params', 'N')
+
+##### check format of pre-defined lead SNPS and genomic regions if files are provided #####
 if leadfile != "NA":
 	leadfile = filedir / "input.lead"
 	delimiter = bh.DetectDelim(open(leadfile).readline())
@@ -50,3 +68,15 @@ if regionfile != "NA":
 	if regionsfile_df_num_of_columns == 0 or regionsfile_df_num_of_columns < 3:
 		sys.exit("Input genomic region file does not have enought columns.")
 	# TODO: add more checks regarding the format of the genomic region file
+
+##### Run GRCh38 #####
+if GRCh38=='1':
+	if chrcol is None or poscol is None or eacol is None or neacol is None:
+		sys.exit("You selected GRCh38 but did not specify chromosome, position, effect allele, or non effect allele")
+	if chrcol is not None and poscol is not None and eacol is not None and neacol is not None:
+		command = "Rscript "+os.path.dirname(os.path.realpath(__file__))+"/giversID.R "+chrcol+" "+poscol+" "+eacol+" "+neacol+" "+filedir+" "+rsIDcol
+		Rsuc=os.system(command)
+		chrcol = "NA"
+		poscol = "NA"
+		rsIDcol = "RSID"
+		bh.grcg38_errors(Rsuc)
--- scripts/gwas_file/src/helpers/basic_helpers.py
+++ scripts/gwas_file/src/helpers/basic_helpers.py
@@ -1,5 +1,6 @@
 import re
 import csv
+import sys
 
 # This function is used to detect the delimiter of a CSV file.
 # The function takes a header parameter, which is assumed to be the first row of a CSV file.
@@ -18,3 +19,14 @@ def DetectDelim(header):
 # this function is used to check if a variable is a float
 def is_float(s):
     return isinstance(s, float)
+
+def grcg38_errors(err_code):
+	if err_code == 512:
+		sys.exit("chr_10001, pos_10001, allele_10001, allele_20001 are in input. Please rename columns to something else.")
+	elif err_code == 768:
+		sys.exit("not all specified columns match input file")
+	elif err_code == 1024:
+		sys.exit("Some column names are duplicated in the input file")
+	elif err_code != 0:
+		sys.exit("Something went wrong when converting GRCh38 to rsID. Please contact the developer.")
+	return err_code
