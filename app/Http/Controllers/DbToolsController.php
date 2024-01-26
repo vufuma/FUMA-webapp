79e3d139f15d709bc4175139d0636c0c619e3fa6
Anastasios Mitsigkolas <tasos1109@gmail.com>
2024-01-26 12:32:04 +0100

    Db tools init
    + route
    + controller
    + index view
    + admin page link

---
+++ app/Http/Controllers/DbToolsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DbToolsController extends Controller
+{
+    public function index()
+    {
+        return view('admin.dbtools.dbtools');
+    }
+}
---
+++ resources/views/admin/dbtools/dbtools.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('stylesheets')
+@endsection
+
+@section('content')
+    <div class="container" style="padding-top: 50px;">
+        <div class="table-title">
+            <div class="row">
+                <div class="col-sm-10">
+                    <h2>Database <b>Tools</b></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    {{-- Imports from the web --}}
+
+    {{-- Hand written ones --}}
+
+    {{-- Imports from the project --}}
+@endsection
