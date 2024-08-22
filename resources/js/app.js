3605b0d7fda5098a5532f5d8aa7c891a5631a441
Baldur van Lew <b.van_lew@lumc.nl>
2024-08-22 12:44:10 +0200

    Initial Vite build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-import './bootstrap';
+// Just doing all imports in app (could move to bootstrap)
+import $ from 'jquery';
+//import * as bootstrap from 'bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap-select';
+import 'jszip';
+import 'pdfmake';
+import 'datatables.net';
+import 'datatables.net-buttons';
+import 'datatables.net-select';
+import 'tree-multiselect';
+import * as d3 from 'd3';
+import 'd3-tip';
+import 'd3-queue';
+import '../css/app.css'
+
+window.$ = $;
+window.jQuery = $;
+window.bootstrap = bootstrap;
+window.d3 = d3;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,42 @@
 import { defineConfig } from 'vite';
+import Inspect from 'vite-plugin-inspect';
 import laravel from 'laravel-vite-plugin';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
     plugins: [
+        nodePolyfills(),
+        Inspect(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    // For running the development server inside the workspace docker
+    // The port needs to be exposed in the compose file
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+            clientPort: 5173,
+        },
+        cors: true
+    },
+    build: {
+        sourcemap: true,
+        write: true,
+        rollupOptions: {
+            // external: [],
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString()
+                    }
+                },
+            },
+        },
+    },
 });
