fcaed5a17303a02de488c4826a69acb137d01918
Baldur van Lew <b.van_lew@lumc.nl>
2024-12-02 18:50:01 +0100

    Migrate button size class

--- resources/js/utils/s2g_results.js
+++ resources/js/utils/s2g_results.js
@@ -428,10 +428,10 @@ export function MAGMA_expPlot(data) {
 
 			// img download buttons
 			$('#' + ds + 'Panel').append('<div id="' + ds + 'Plot">Download the plot as '
-				+ '<button class="btn btn-default btn-xs ImgDown" onclick=' + "'" + 'expImgDown("' + ds + '","png");' + "'" + '>PNG</button> '
-				+ '<button class="btn btn-default btn-xs ImgDown" onclick=' + "'" + 'expImgDown("' + ds + '","jpeg");' + "'" + '>JPG</button> '
-				+ '<button class="btn btn-default btn-xs ImgDown" onclick=' + "'" + 'expImgDown("' + ds + '","svg");' + "'" + '>SVG</button> '
-				+ '<button class="btn btn-default btn-xs ImgDown" onclick=' + "'" + 'expImgDown("' + ds + '","pdf");' + "'" + '>PDF</button></div>'
+				+ '<button class="btn btn-default btn-sm ImgDown" onclick=' + "'" + 'expImgDown("' + ds + '","png");' + "'" + '>PNG</button> '
+				+ '<button class="btn btn-default btn-sm ImgDown" onclick=' + "'" + 'expImgDown("' + ds + '","jpeg");' + "'" + '>JPG</button> '
+				+ '<button class="btn btn-default btn-sm ImgDown" onclick=' + "'" + 'expImgDown("' + ds + '","svg");' + "'" + '>SVG</button> '
+				+ '<button class="btn btn-default btn-sm ImgDown" onclick=' + "'" + 'expImgDown("' + ds + '","pdf");' + "'" + '>PDF</button></div>'
 			);
 
 			// plot
