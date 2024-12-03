ec359a8704bae31bb4561a01fd253262630c2e75
Baldur van Lew <b.van_lew@lumc.nl>
2024-12-03 17:19:24 +0100

    Realign footer components

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,20 +1,19 @@
 <div class="container-fluid">
     <div class="row" style="color: #818588;">
-        <div class="col-md-4">
-            <div class="mx-auto" style="width: 300px;">
-                <p style="font-size:16px">
-                    Update/maintenance: FUMA team<br> 
-                    (fuma.ctglab@gmail.com)<br>
-                    Dept. Complex Trait Genetics, <br>VU University Amsterdam
-                </p>
-            </div>
+        <div class="col-md-3 offset-md-2">
+            <p style="font-size:16px">
+                Update/maintenance: FUMA team<br>
+                (fuma.ctglab@gmail.com)<br>
+                Dept. Complex Trait Genetics, <br>VU University Amsterdam
+            </p>
         </div>
         <div class="col-md-4">
             <div class="mx-auto" style="width: 300px; display: flex; justify-content: center; align-items: center;">
-                <img class="footerimg" src="{{URL::asset('/image/ctg.svg')}}" height="70" width="180" alt="CNCR CTGLab">
+                <img class="footerimg" src="{{ URL::asset('/image/ctg.svg') }}" height="70" width="180"
+                    alt="CNCR CTGLab">
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="mx-auto" style="width: 300px;">
                 <p style="font-size:16px">
                     <br>Developed by:<br> Kyoko Watanabe
