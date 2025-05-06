e71a044542ca330934588487f4c6539bbddfa6b0
Baldur van Lew <b.van_lew@lumc.nl>
2025-05-06 17:47:31 +0200

    Fixed missing default export bug
    Corrected usage of swal (sweet alert) to conform to new async api.
    Added more informative error alert on fail.
    Set subdir to ""
    TODO: think about removing subdir. AFAIK it is blank in all pages

--- resources/js/utils/jobsSearch.js
+++ resources/js/utils/jobsSearch.js
@@ -1,39 +1,45 @@
 import swal from 'sweetalert';
 var prefix = "jobs";
 export function setupDeleteJob() {
-	$('#deleteJob').on('click', function () {
-		swal({
+	$('#deleteJob').on('click', async() => {
+		var span = document.createElement("span");
+		span.innerHTML = "Do you really want to remove selected jobs?<br><div class='alert alert-danger'>If you have selected a public job, it will be permanently deleted from the public list.</div>";
+		let isConfirm = await swal({
 			title: "Are you sure?",
-			html: true,
-			text: "Do you really want to remove selected jobs?",
-			type: "warning",
-			showCancelButton: true,
-			closeOnConfirm: true,
-		}).then((isConfirm) => {
-			if (isConfirm) {
-				$('.deleteJobCheck').each(function () {
-					if ($(this).is(":checked")) {
-						$.ajax({
-							url: subdir + '/' + $(this).attr('job-type') + '/deleteJob',
-							type: "POST",
-							data: {
-								jobID: $(this).attr('job-id')
-							},
-							error: function () {
-								alert("error at deleteJob");
-							},
-							success: function (resdata) {
-								// chech if resdata is null
-								if (resdata != "") {
-									alert(resdata);
-								}
+			content: span,
+			icon: "warning",
+			buttons: {
+				cancel: true,
+				confirm: true,
+			}
+		});
+		if (isConfirm) {
+			$('.deleteJobCheck').each(function () {
+				if ($(this).is(":checked")) {
+					let subdir = ""; 
+					$.ajax({
+						url: subdir + '/' + $(this).attr('job-type') + '/deleteJob',
+						type: "POST",
+						data: {
+							jobID: $(this).attr('job-id')
+						},
+						error: function (xhr, status, error) {
+							var err = JSON.parse(xhr.responseText);
+							alert(`Error at deleteJob: ${err.message}`);
+						},
+						success: function (resdata) {
+							// chech if resdata is null
+							if (resdata != "") {
+								alert(resdata);
 							}
-						});
-					}
-				});
-            }
-        });
+						}
+					});
+				}
+			});
+		};
 	}
 )}
+
+export default setupDeleteJob;
 
 
