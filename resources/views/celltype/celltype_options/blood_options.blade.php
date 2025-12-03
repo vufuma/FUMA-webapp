4892929cd3c49303d855275039bfd315b3de1e85
tanyaphung <tanyaphung>
2025-12-03 14:02:53 +0100

    temp save refactoring blade to load all of the scRNAseq datasets

---
+++ resources/views/celltype/celltype_options/blood_options.blade.php
@@ -0,0 +1,6 @@
+<option value="GSE89232_Human_Blood" data-section="Blood/Human" data-key="0">
+    GSE89232_Human_Blood
+</option>
+<option value="MouseCellAtlas_Peripheral_Blood" data-section="Blood/Mouse" data-key="0">
+    MouseCellAtlas_Peripheral_Blood
+</option>
---
+++ resources/views/celltype/celltype_options/boneMarrow_options.blade.php
@@ -0,0 +1,9 @@
+<option value="MouseCellAtlas_Bone_Marrow" data-section="Bone Marrow/Mouse" data-key="0">
+    MouseCellAtlas_Bone_Marrow
+</option>
+<option value="TabulaMuris_FACS_Marrow" data-section="Bone Marrow/Mouse" data-key="1">
+    TabulaMuris_FACS_Marrow
+</option>
+<option value="TabulaMuris_droplet_Marrow" data-section="Bone Marrow/Mouse"
+    data-key="2">TabulaMuris_droplet_Marrow
+</option>
