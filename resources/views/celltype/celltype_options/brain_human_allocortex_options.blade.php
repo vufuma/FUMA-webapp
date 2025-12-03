fa36615249c69736f9905a309e0a681bd434acd9
tanyaphung <tanyaphung>
2025-12-03 22:01:10 +0100

    edit the options for brain

--- resources/views/celltype/celltype_options/brain_human_allocortex_options.blade.php
+++ resources/views/celltype/celltype_options/brain_human_allocortex_options.blade.php
@@ -1,89 +1,114 @@
-<!-- prenatal -->
+<!-- prenatal level 1-->
 <option value="254_Smith2021_MidgestationalNeocortex_Human_2021_HippocampalFormation_level1"
-    data-section="Brain/Human/Allocortex/Prenatal" data-key="0">
-    254_Smith2021_MidgestationalNeocortex_Human_2021_HippocampalFormation_level1</option>
+data-section="Brain/Human/Allocortex/Prenatal/Level1" data-key="0">
+254_Smith2021_MidgestationalNeocortex_Human_2021_HippocampalFormation_level1</option>
 
 <!-- postnatal level 1 -->
 <option value="20_Siletti_CerebralCortex.Pir_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="0">
-    20_Siletti_CerebralCortex.Pir_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="0">
+20_Siletti_CerebralCortex.Pir_Human_2022_level1</option>
+
 <option value="32_Siletti_CerebralCortex.AON_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="1">
-    32_Siletti_CerebralCortex.AON_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="1">
+32_Siletti_CerebralCortex.AON_Human_2022_level1</option>
+
 <option value="59_Siletti_Hippocampus.HiT.CA4-DGC_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="2">
-    59_Siletti_Hippocampus.HiT.CA4-DGC_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="2">
+59_Siletti_Hippocampus.HiT.CA4-DGC_Human_2022_level1</option>
+
 <option value="60_Siletti_Hippocampus.HiH.HiT.Sub_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="3">
-    60_Siletti_Hippocampus.HiH.HiT.Sub_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="3">
+60_Siletti_Hippocampus.HiH.HiT.Sub_Human_2022_level1</option>
+
 <option value="61_Siletti_Hippocampus.HiH.CA1_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="4">
-    61_Siletti_Hippocampus.HiH.CA1_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="4">
+61_Siletti_Hippocampus.HiH.CA1_Human_2022_level1</option>
+
 <option value="62_Siletti_Hippocampus.HiH.CA1-3_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="5">
-    62_Siletti_Hippocampus.HiH.CA1-3_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="5">
+62_Siletti_Hippocampus.HiH.CA1-3_Human_2022_level1</option>
+
 <option value="63_Siletti_Hippocampus.HiH.CA1-CA3_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="6">
-    63_Siletti_Hippocampus.HiH.CA1-CA3_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="6">
+63_Siletti_Hippocampus.HiH.CA1-CA3_Human_2022_level1</option>
+
 <option value="64_Siletti_Hippocampus.HiH.DG-CA4_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="7">
-    64_Siletti_Hippocampus.HiH.DG-CA4_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="7">
+64_Siletti_Hippocampus.HiH.DG-CA4_Human_2022_level1</option>
+
 <option value="65_Siletti_Hippocampus.HiB-RostralCA1-CA3_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="8">
-    65_Siletti_Hippocampus.HiB-RostralCA1-CA3_Human_2022_level1</option> 
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="8">
+65_Siletti_Hippocampus.HiB-RostralCA1-CA3_Human_2022_level1</option> 
+
 <option value="66_Siletti_Hippocampus.HiH.CA2-3_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="9">
-    66_Siletti_Hippocampus.HiH.CA2-3_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="9">
+66_Siletti_Hippocampus.HiH.CA2-3_Human_2022_level1</option>
+
 <option value="67_Siletti_Hippocampus.HiB.RostralCA1-2_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="10">
-    67_Siletti_Hippocampus.HiB.RostralCA1-2_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="10">
+67_Siletti_Hippocampus.HiB.RostralCA1-2_Human_2022_level1</option>
+
 <option value="68_Siletti_Hippocampus.HiB.RostralDG-CA4_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="11">
-    68_Siletti_Hippocampus.HiB.RostralDG-CA4_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="11">
+68_Siletti_Hippocampus.HiB.RostralDG-CA4_Human_2022_level1</option>
+
 <option value="69_Siletti_Hippocampus.HiB.RostralCA3_Human_2022_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="12">
-    69_Siletti_Hippocampus.HiB.RostralCA3_Human_2022_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="12">
+69_Siletti_Hippocampus.HiB.RostralCA3_Human_2022_level1</option>
+
 <option value="549_Xu_Human_2023_Hippocampus_level1"
-    data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="13">
-    549_Xu_Human_2023_Hippocampus_level1</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level1" data-key="13">
+549_Xu_Human_2023_Hippocampus_level1</option>
 
 <!-- postnatal level 2 -->
 <option value="20_Siletti_CerebralCortex.Pir_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="0">
-    20_Siletti_CerebralCortex.Pir_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="0">
+20_Siletti_CerebralCortex.Pir_Human_2022_level2</option>
+
 <option value="32_Siletti_CerebralCortex.AON_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="1">
-    32_Siletti_CerebralCortex.AON_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="1">
+32_Siletti_CerebralCortex.AON_Human_2022_level2</option>
+
 <option value="59_Siletti_Hippocampus.HiT.CA4-DGC_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="2">
-    59_Siletti_Hippocampus.HiT.CA4-DGC_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="2">
+59_Siletti_Hippocampus.HiT.CA4-DGC_Human_2022_level2</option>
+
 <option value="60_Siletti_Hippocampus.HiH.HiT.Sub_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="3">
-    60_Siletti_Hippocampus.HiH.HiT.Sub_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="3">
+60_Siletti_Hippocampus.HiH.HiT.Sub_Human_2022_level2</option>
+
 <option value="61_Siletti_Hippocampus.HiH.CA1_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="4">
-    61_Siletti_Hippocampus.HiH.CA1_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="4">
+61_Siletti_Hippocampus.HiH.CA1_Human_2022_level2</option>
+
 <option value="62_Siletti_Hippocampus.HiH.CA1-3_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="5">
-    62_Siletti_Hippocampus.HiH.CA1-3_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="5">
+62_Siletti_Hippocampus.HiH.CA1-3_Human_2022_level2</option>
+
 <option value="63_Siletti_Hippocampus.HiH.CA1-CA3_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="6">
-    63_Siletti_Hippocampus.HiH.CA1-CA3_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="6">
+63_Siletti_Hippocampus.HiH.CA1-CA3_Human_2022_level2</option>
+
 <option value="64_Siletti_Hippocampus.HiH.DG-CA4_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="7">
-    64_Siletti_Hippocampus.HiH.DG-CA4_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="7">
+64_Siletti_Hippocampus.HiH.DG-CA4_Human_2022_level2</option>
+
 <option value="65_Siletti_Hippocampus.HiB-RostralCA1-CA3_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="8">
-    65_Siletti_Hippocampus.HiB-RostralCA1-CA3_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="8">
+65_Siletti_Hippocampus.HiB-RostralCA1-CA3_Human_2022_level2</option>
+
 <option value="66_Siletti_Hippocampus.HiH.CA2-3_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="9">
-    66_Siletti_Hippocampus.HiH.CA2-3_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="9">
+66_Siletti_Hippocampus.HiH.CA2-3_Human_2022_level2</option>
+
 <option value="67_Siletti_Hippocampus.HiB.RostralCA1-2_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="10">
-    67_Siletti_Hippocampus.HiB.RostralCA1-2_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="10">
+67_Siletti_Hippocampus.HiB.RostralCA1-2_Human_2022_level2</option>
+
 <option value="68_Siletti_Hippocampus.HiB.RostralDG-CA4_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="11">
-    68_Siletti_Hippocampus.HiB.RostralDG-CA4_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="11">
+68_Siletti_Hippocampus.HiB.RostralDG-CA4_Human_2022_level2</option>
+
 <option value="69_Siletti_Hippocampus.HiB.RostralCA3_Human_2022_level2"
-    data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="12">
-    69_Siletti_Hippocampus.HiB.RostralCA3_Human_2022_level2</option>
+data-section="Brain/Human/Allocortex/Postnatal/Level2" data-key="12">
+69_Siletti_Hippocampus.HiB.RostralCA3_Human_2022_level2</option>
---
+++ resources/views/celltype/celltype_options/brain_human_cerebralGyriAndLobules_options.blade.php
@@ -0,0 +1,77 @@
+<!-- prenatal level 1 -->
+<option value="387_Velmeshev2023_PrePostNatal_Human_2023_group2_STG_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Prenatal/Level1" data-key="0">
+387_Velmeshev2023_PrePostNatal_Human_2023_group2_STG_level1</option>
+
+<option value="253_Smith2021_MidgestationalNeocortex_Human_2021_ParietalLobe_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Prenatal/Level1" data-key="1">
+253_Smith2021_MidgestationalNeocortex_Human_2021_ParietalLobe_level1</option>
+
+<!-- prenatal level 2 -->
+
+<!-- postnatal level 1 -->
+<option value="261_Smith2021_InfantNeocortex_Human_2021_TemporalLobe_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="0">
+261_Smith2021_InfantNeocortex_Human_2021_TemporalLobe_level1</option>
+
+<option value="262_Smith2021_InfantNeocortex_Human_2021_ParietalLobe_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="1">
+262_Smith2021_InfantNeocortex_Human_2021_ParietalLobe_level1</option>
+
+<option value="12_Siletti_CerebralCortex.STG_Human_2022_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="2">
+12_Siletti_CerebralCortex.STG_Human_2022_level1</option>
+
+<option value="31_Siletti_CerebralCortex.FuGt.TF_Human_2022_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="3">
+31_Siletti_CerebralCortex.FuGt.TF_Human_2022_level1</option>
+
+<option value="35_Siletti_CerebralCortex.ITG_Human_2022_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="4">
+35_Siletti_CerebralCortex.ITG_Human_2022_level1</option>
+
+<option value="325_Jorstad2023_AnG_Human_2023_10x_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="5">
+325_Jorstad2023_AnG_Human_2023_10x_level1</option>
+
+<option value="11_Siletti_CerebralCortex.PPH.TH-TL_Human_2022_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="6">
+11_Siletti_CerebralCortex.PPH.TH-TL_Human_2022_level1</option>
+
+<option value="425_Leng2021_Human_2021_SFG_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="7">
+425_Leng2021_Human_2021_SFG_level1</option>
+
+<option value="34_Siletti_CerebralCortex.SOG.A19_Human_2022_level1"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level1" data-key="8">
+34_Siletti_CerebralCortex.SOG.A19_Human_2022_level1</option>
+
+<!-- postnatal level 2 -->
+<option value="12_Siletti_CerebralCortex.STG_Human_2022_level2"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level2" data-key="0">
+12_Siletti_CerebralCortex.STG_Human_2022_level2</option>
+
+<option value="31_Siletti_CerebralCortex.FuGt.TF_Human_2022_level2"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level2" data-key="1">
+31_Siletti_CerebralCortex.FuGt.TF_Human_2022_level2</option>
+
+<option value="35_Siletti_CerebralCortex.ITG_Human_2022_level2"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level2" data-key="2">
+35_Siletti_CerebralCortex.ITG_Human_2022_level2</option>
+
+<option value="325_Jorstad2023_AnG_Human_2023_10x_level2"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level2" data-key="3">
+325_Jorstad2023_AnG_Human_2023_10x_level2</option>
+
+<option value="11_Siletti_CerebralCortex.PPH.TH-TL_Human_2022_level2"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level2" data-key="4">
+11_Siletti_CerebralCortex.PPH.TH-TL_Human_2022_level2</option>
+
+<option value="34_Siletti_CerebralCortex.SOG.A19_Human_2022_level2"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level2" data-key="5">
+34_Siletti_CerebralCortex.SOG.A19_Human_2022_level2</option>
+
+<!-- postnatal level 3 -->
+<option value="325_Jorstad2023_AnG_Human_2023_10x_level3"
+data-section="Brain/Human/Cerebral Gyri And Lobules/Postnatal/Level3" data-key="0">
+325_Jorstad2023_AnG_Human_2023_10x_level3</option>
