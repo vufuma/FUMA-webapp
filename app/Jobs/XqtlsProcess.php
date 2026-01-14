b447abb6b841f44eb9bccc52dfdfdf2b263bba9d
tanyaphung <tanyaphung>
2026-01-14 12:41:46 +0100

    add coloc docker command and update jobs to ok

--- app/Jobs/XqtlsProcess.php
+++ app/Jobs/XqtlsProcess.php
@@ -49,29 +49,44 @@ class XqtlsProcess implements ShouldQueue
 
         $filedir = config('app.jobdir') . '/xqtls/' . $jobID . '/';
         $ref_data_path_on_host = config('app.ref_data_on_host_path');
+        $params = parse_ini_string(Storage::get($filedir . "params.config"), false, INI_SCANNER_RAW);
         $this->logfile = $filedir . "job.log";
         $this->errorfile = $filedir . "error.log";
 
-        $container_name = DockerNamesBuilder::containerName($jobID);
-        $image_name = DockerNamesBuilder::imageName('laradock-fuma-js', 'xqtls');
-        $job_location = DockerNamesBuilder::jobLocation($jobID, 'xqtls');
+        if ($params['coloc'] == 1) {
 
-        $cmd_format = "docker run --rm --net=none --name " . $container_name . " -v $ref_data_path_on_host:/data -v " . config('app.abs_path_to_jobs_dir_on_host') . ":" . config('app.abs_path_to_jobs_dir_on_host') . " " . $image_name . " /bin/sh -c 'python format_for_lava_coloc.py --filedir $job_location/ >>$job_location/job.log 2>>$job_location/error.log'";
-        $process_format = Process::forever()->run($cmd_format);
-        Log::info("Full Docker command: " . $cmd_format);
-        Storage::append($this->logfile, "Command to be executed:");
-        Storage::append($this->logfile, $cmd_format . "\n");
+            $container_name = DockerNamesBuilder::containerName($jobID);
+            $image_name = DockerNamesBuilder::imageName('laradock-fuma-js', 'xqtls');
+            $job_location = DockerNamesBuilder::jobLocation($jobID, 'xqtls');
 
-        $cmd_coloc = "docker run --rm --net=none --name " . $container_name . " -v $ref_data_path_on_host:/data -v " . config('app.abs_path_to_jobs_dir_on_host') . ":" . config('app.abs_path_to_jobs_dir_on_host') . " " . $image_name . " /bin/sh -c '/home/tnphung/miniconda3/bin/Rscript run_coloc.R --filedir $job_location/ >>$job_location/job.log 2>>$job_location/error.log'";
-        $process_coloc = Process::forever()->run($cmd_coloc);
-        Log::info("Full Docker command: " . $cmd_coloc);
-        Storage::append($this->logfile, "Command to be executed:");
-        Storage::append($this->logfile, $cmd_coloc . "\n");
-        $error = $process_format->exitCode();
-        if ($error != 0) {
-            JobHelper::JobTerminationHandling($jobID, 1, 'xqtls error occured');
-            return;
+            $cmd_format = "docker run --rm --net=none --name " . $container_name . " -v $ref_data_path_on_host:/data -v " . config('app.abs_path_to_jobs_dir_on_host') . ":" . config('app.abs_path_to_jobs_dir_on_host') . " " . $image_name . " /bin/sh -c 'python3 format_for_lava_coloc.py --filedir $job_location/ >>$job_location/job.log 2>>$job_location/error.log'";
+            $process_format = Process::forever()->run($cmd_format);
+            Log::info("Full Docker command: " . $cmd_format);
+            Storage::append($this->logfile, "Command to be executed:");
+            Storage::append($this->logfile, $cmd_format . "\n");
+
+            $cmd_coloc = "docker run --rm --net=none --name " . $container_name . " -v $ref_data_path_on_host:/data -v " . config('app.abs_path_to_jobs_dir_on_host') . ":" . config('app.abs_path_to_jobs_dir_on_host') . " " . $image_name . " /bin/sh -c 'Rscript run_coloc.R --filedir $job_location/ >>$job_location/job.log 2>>$job_location/error.log'";
+            $process_coloc = Process::forever()->run($cmd_coloc);
+            Log::info("Full Docker command: " . $cmd_coloc);
+            Storage::append($this->logfile, "Command to be executed:");
+            Storage::append($this->logfile, $cmd_coloc . "\n");
+            $error = $process_format->exitCode();
+            if ($error != 0) {
+                JobHelper::JobTerminationHandling($jobID, 1, 'xqtls error occured');
+                return;
+            }
+
+            SubmitJob::where('jobID', $jobID) 
+            ->update([
+                'status' => config('snp2gene_status_codes.15.short_name'),
+                'completed_at' => date("Y-m-d H:i:s")
+            ]);
+
+        } else
+        {
+            Storage::append($this->logfile, "Colocalization analysis not selected.\n");
         }
+
     }
 
 }
