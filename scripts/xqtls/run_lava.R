73b3e38d6e070dc59d8daff5df5e46bcc66d727a
tanyaphung <tanyaphung>
2026-02-04 10:50:59 +0100

    temporary fix of lava

--- scripts/xqtls/run_lava.R
+++ scripts/xqtls/run_lava.R
@@ -110,14 +110,12 @@ colnames(results) = c("locus", "chr", "phen1", "rho", "rho.lower", "rho.upper", 
 
 
 # ## process input
-input_shared = process.input(input.info.file = paste0(filedir, filename),
+for (dataset in unlist(strsplit(datasets, ":"))) {
+  ## process input
+  input = process.input(input.info.file = paste0(filedir, filename),
                       sample.overlap.file=NULL,
                       ref.prefix = paste0(lava_dir, "/g1000_eur"),
                       phenos=c(phenotype))
-
-for (dataset in unlist(strsplit(datasets, ":"))) {
-  ## process input
-  input = input_shared
 
   ## set up output file names
   out.fname = paste0("lava_results_", dataset)
@@ -130,84 +128,84 @@ for (dataset in unlist(strsplit(datasets, ":"))) {
   process.eqtl.input(input, qtl_fn, chromosomes=chrom, sample.size=sample_size)
 
   ### Set univariate pvalue threshold
-    univ.p.thresh = 1e-4
+  univ.p.thresh = 1e-4
 
-    ### Run LAVA analysis
-    u=b=list()
+  ### Run LAVA analysis
+  u=b=list()
 
-    if (tolower(lavaGene) == "all") {
-      genes_to_test <- input$current.genes
-    } else {
-      genes_to_test <- unlist(strsplit(lavaGene, ","))
-      genes_to_test <- trimws(genes_to_test)
-      genes_to_test <- intersect(genes_to_test, input$current.genes)
-    }
+  if (tolower(lavaGene) == "all") {
+    genes_to_test <- input$current.genes
+  } else {
+    genes_to_test <- unlist(strsplit(lavaGene, ","))
+    genes_to_test <- trimws(genes_to_test)
+    genes_to_test <- intersect(genes_to_test, input$current.genes)
+  }
 
-    # ### if a gene is specified, filter the input to only include that gene
-    # if (lavaGene != "NA") {
-    #   i = lavaGene
-    #   locus = process.eqtl.locus(i, input) # process locus
-    
-    #   # It is possible that the locus cannot be defined for various reasons (e.g. too few SNPs), so the !is.null(locus) check is necessary before calling the analysis functions.
-    #   if (!is.null(locus)) {
-    #       # extract some general locus info for the output
-    #       loc.info = data.frame(locus = locus$id, chr = locus$chr)
-          
-    #       # run the univariate and bivariate tests
-    #       loc.out = run.univ.bivar(locus, univ.thresh = univ.p.thresh)
-    #       u[[i]] = cbind(loc.info, loc.out$univ)
-    #       if(!is.null(loc.out$bivar)) b[[i]] = cbind(loc.info, loc.out$bivar)
-    #   }
-    #   else {
-    #     print(paste0("Locus for gene ", lavaGene, " could not be defined. Skipping LAVA analysis for this gene."))
-    #     quit(status=3) #TODO: check this functionality works as intended
-    #   }
-    # } else {
-      for (i in genes_to_test) {
-        locus = process.eqtl.locus(i, input)                                      # process locus
+  # ### if a gene is specified, filter the input to only include that gene
+  # if (lavaGene != "NA") {
+  #   i = lavaGene
+  #   locus = process.eqtl.locus(i, input) # process locus
+  
+  #   # It is possible that the locus cannot be defined for various reasons (e.g. too few SNPs), so the !is.null(locus) check is necessary before calling the analysis functions.
+  #   if (!is.null(locus)) {
+  #       # extract some general locus info for the output
+  #       loc.info = data.frame(locus = locus$id, chr = locus$chr)
         
-        # It is possible that the locus cannot be defined for various reasons (e.g. too few SNPs), so the !is.null(locus) check is necessary before calling the analysis functions.
-        if (!is.null(locus)) {
-            # extract some general locus info for the output
-            loc.info = data.frame(locus = locus$id, chr = locus$chr)
-            
-            # run the univariate and bivariate tests
-            loc.out = run.univ.bivar(locus, univ.thresh = univ.p.thresh)
-            u[[i]] = cbind(loc.info, loc.out$univ)
-            if(!is.null(loc.out$bivar)) b[[i]] = cbind(loc.info, loc.out$bivar)
-        }
-    }
-    # }
+  #       # run the univariate and bivariate tests
+  #       loc.out = run.univ.bivar(locus, univ.thresh = univ.p.thresh)
+  #       u[[i]] = cbind(loc.info, loc.out$univ)
+  #       if(!is.null(loc.out$bivar)) b[[i]] = cbind(loc.info, loc.out$bivar)
+  #   }
+  #   else {
+  #     print(paste0("Locus for gene ", lavaGene, " could not be defined. Skipping LAVA analysis for this gene."))
+  #     quit(status=3) #TODO: check this functionality works as intended
+  #   }
+  # } else {
+    for (i in genes_to_test) {
+      locus = process.eqtl.locus(i, input)                                      # process locus
+      
+      # It is possible that the locus cannot be defined for various reasons (e.g. too few SNPs), so the !is.null(locus) check is necessary before calling the analysis functions.
+      if (!is.null(locus)) {
+          # extract some general locus info for the output
+          loc.info = data.frame(locus = locus$id, chr = locus$chr)
+          
+          # run the univariate and bivariate tests
+          loc.out = run.univ.bivar(locus, univ.thresh = univ.p.thresh)
+          u[[i]] = cbind(loc.info, loc.out$univ)
+          if(!is.null(loc.out$bivar)) b[[i]] = cbind(loc.info, loc.out$bivar)
+      }
+  }
+  # }
 
-    
+  
 
-    # save the output
-    write.table(do.call(rbind,u), paste0(filedir, out.fname,".univ.lava"), row.names=F,quote=F,col.names=T)
-    write.table(do.call(rbind,b), paste0(filedir, out.fname,".bivar.tmp.lava"), row.names=F,quote=F,col.names=T)
+  # save the output
+  write.table(do.call(rbind,u), paste0(filedir, out.fname,".univ.lava"), row.names=F,quote=F,col.names=T)
+  write.table(do.call(rbind,b), paste0(filedir, out.fname,".bivar.tmp.lava"), row.names=F,quote=F,col.names=T)
 
-    print(paste0("Done! Analysis output written to ",out.fname,".*.lava"))
+  print(paste0("Done! Analysis output written to ",out.fname,".*.lava"))
 
-    # add bonferroni corrected p value
-    bivar_results = fread(paste0(filedir, out.fname,".bivar.tmp.lava"))
-    bivar_results = bivar_results %>%
-      select(-c("phen2"))
-    bivar_results$dataset = dataset
-    bivar_results$p.adjust = p.adjust(bivar_results$p, method = "bonferroni")
+  # add bonferroni corrected p value
+  bivar_results = fread(paste0(filedir, out.fname,".bivar.tmp.lava"))
+  bivar_results = bivar_results %>%
+    select(-c("phen2"))
+  bivar_results$dataset = dataset
+  bivar_results$p.adjust = p.adjust(bivar_results$p, method = "bonferroni")
 
-    # add gene symbol column
-    gene_conversion_orig = fread(paste0(gene_conversion_dir, "/gencode.v39.gene_name_conversion.tsv")) #TODO: make this dynamic
-    gene_conversion = gene_conversion_orig %>%
-      select("gene_id", "gene_name") %>% unique()
-    colnames(gene_conversion) = c("locus", "symbol")
-    bivar_results = bivar_results %>%
-      left_join(gene_conversion, by="locus")
+  # add gene symbol column
+  gene_conversion_orig = fread(paste0(gene_conversion_dir, "/gencode.v39.gene_name_conversion.tsv")) #TODO: make this dynamic
+  gene_conversion = gene_conversion_orig %>%
+    select("gene_id", "gene_name") %>% unique()
+  colnames(gene_conversion) = c("locus", "symbol")
+  bivar_results = bivar_results %>%
+    left_join(gene_conversion, by="locus")
 
-    write.table(bivar_results, paste0(filedir, out.fname,".bivar.lava"), row.names=F, quote=F, col.names=T)
+  write.table(bivar_results, paste0(filedir, out.fname,".bivar.lava"), row.names=F, quote=F, col.names=T)
 
-    # combine results from different datasets
-    results = rbind(results, as.data.frame(bivar_results))
+  # combine results from different datasets
+  results = rbind(results, as.data.frame(bivar_results))
 
-    rm(input)
+  rm(input)
 }
 
 write.table(results, file=paste0(filedir, "lava_bivar_results_all_datasets.txt"), row.names=F, quote=F, col.names=T, sep="\t")
