8a374b79ce4f537fb4fca6da4d3b819205ecd999
tanyaphung <tanyaphung>
2026-04-17 13:12:54 +0200

    update the path to pqtls

--- resources/views/snp2gene/xqtls_options/pqtls/_pqtl_plasma_options.blade.php
+++ resources/views/snp2gene/xqtls_options/pqtls/_pqtl_plasma_options.blade.php
@@ -1,48 +1,48 @@
 <option class="level1" value="null"> 1_suhre_2017 (1) </option>
-<option class="level2" value="pQTL/1_suhre_2017/1_suhre_2017.txt.gz">KORA Plasma</option>
+<option class="level2" value="pQTL/1_suhre_2017/sig_pairs/1_suhre_2017.txt.gz">KORA Plasma</option>
 
 <option class="level1" value="null"> 2_sun_2016 (1) </option>
-<option class="level2" value="pQTL/2_sun_2016/2_sun_2016_meta.txt.gz">SPIROMICS COPDGene Plasma</option>
+<option class="level2" value="pQTL/2_sun_2016/sig_pairs/2_sun_2016_meta.txt.gz">SPIROMICS COPDGene Plasma</option>
 
 <option class="level1" value="null"> 3_gudjonsson_2022 (1) </option>
-<option class="level2" value="pQTL/3_gudjonsson_2022/3_gudjonsson_2022.txt.gz">AGES-Reykjavik Serum</option>
+<option class="level2" value="pQTL/3_gudjonsson_2022/sig_pairs/3_gudjonsson_2022.txt.gz">AGES-Reykjavik Serum</option>
 
 <option class="level1" value="null"> 4_sun_2018 (1) </option>
-<option class="level2" value="pQTL/4_sun_2018/4_sun_2018.txt.gz">INTERVAL Plasma</option>
+<option class="level2" value="pQTL/4_sun_2018/sig_pairs/4_sun_2018.txt.gz">INTERVAL Plasma</option>
 
 <option class="level1" value="null"> 5_emilsson_2022 (2) </option>
-<option class="level2" value="pQTL/5_emilsson_2022/5_emilsson_2022_noConditional.txt.gz">AGES-Reykjavik Serum No Conditional</option>
-<option class="level2" value="pQTL/5_emilsson_2022/5_emilsson_2022_conditional.txt.gz">AGES-Reykjavik Serum Conditional</option>
+<option class="level2" value="pQTL/5_emilsson_2022/sig_pairs/5_emilsson_2022_noConditional.txt.gz">AGES-Reykjavik Serum No Conditional</option>
+<option class="level2" value="pQTL/5_emilsson_2022/sig_pairs/5_emilsson_2022_conditional.txt.gz">AGES-Reykjavik Serum Conditional</option>
 
 <option class="level1" value="null"> 6_katz_2021 (2) </option>
-<option class="level2" value="pQTL/6_katz_2021/6_katz_2021_discovery.txt.gz">JacksonHeart Plasma Discovery</option>
-<option class="level2" value="pQTL/6_katz_2021/6_katz_2021_meta.txt.gz">JacksonHeart Plasma Meta</option>
+<option class="level2" value="pQTL/6_katz_2021/sig_pairs/6_katz_2021_discovery.txt.gz">JacksonHeart Plasma Discovery</option>
+<option class="level2" value="pQTL/6_katz_2021/sig_pairs/6_katz_2021_meta.txt.gz">JacksonHeart Plasma Meta</option>
 
 <option class="level1" value="null"> 7_ferkingstad_2021 (1) </option>
-<option class="level2" value="pQTL/7_ferkingstad_2021/7_ferkingstad_2021.txt.gz">IcelandicCancer deCODE Plasma</option>
+<option class="level2" value="pQTL/7_ferkingstad_2021/sig_pairs/7_ferkingstad_2021.txt.gz">IcelandicCancer deCODE Plasma</option>
 
 <option class="level1" value="null"> 8_pietzner_2021 (1) </option>
-<option class="level2" value="pQTL/8_pietzner_2021/8_pietzner_2021.txt.gz">Fenland Plasma</option>
+<option class="level2" value="pQTL/8_pietzner_2021/sig_pairs/8_pietzner_2021.txt.gz">Fenland Plasma</option>
 
 <option class="level1" value="null"> 9_sun_2023 (6) </option>
-<option class="level2" value="pQTL/9_sun_2023/9_sun_2023_discovery.txt.gz">UKB Plasma Discovery</option>
-<option class="level2" value="pQTL/9_sun_2023/9_sun_2023_combined.txt.gz">UKB Plasma Combined</option>
-<option class="level2" value="pQTL/9_sun_2023/9_sun_2023_MID.txt.gz">UKB Plasma Middle Eastern</option>
-<option class="level2" value="pQTL/9_sun_2023/9_sun_2023_EAS.txt.gz">UKB Plasma East Asian</option>
-<option class="level2" value="pQTL/9_sun_2023/9_sun_2023_CSA.txt.gz">UKB Plasma Central South Asian</option>
-<option class="level2" value="pQTL/9_sun_2023/9_sun_2023_AFR.txt.gz">UKB Plasma African</option>
+<option class="level2" value="pQTL/9_sun_2023/sig_pairs/9_sun_2023_discovery.txt.gz">UKB Plasma Discovery</option>
+<option class="level2" value="pQTL/9_sun_2023/sig_pairs/9_sun_2023_combined.txt.gz">UKB Plasma Combined</option>
+<option class="level2" value="pQTL/9_sun_2023/sig_pairs/9_sun_2023_MID.txt.gz">UKB Plasma Middle Eastern</option>
+<option class="level2" value="pQTL/9_sun_2023/sig_pairs/9_sun_2023_EAS.txt.gz">UKB Plasma East Asian</option>
+<option class="level2" value="pQTL/9_sun_2023/sig_pairs/9_sun_2023_CSA.txt.gz">UKB Plasma Central South Asian</option>
+<option class="level2" value="pQTL/9_sun_2023/sig_pairs/9_sun_2023_AFR.txt.gz">UKB Plasma African</option>
 
 <option class="level1" value="null"> 10_carland_2023 (3) </option>
-<option class="level2" value="pQTL/10_carland_2023/10_carland_2023_pooled.txt.gz">Meta 12 cohorts Plasma Pooled</option>
-<option class="level2" value="pQTL/10_carland_2023/10_carland_2023_male.txt.gz">Meta 12 cohorts Plasma Male</option>
-<option class="level2" value="pQTL/10_carland_2023/10_carland_2023_female.txt.gz">Meta 12 cohorts Plasma Female</option>
+<option class="level2" value="pQTL/10_carland_2023/sig_pairs/10_carland_2023_pooled.txt.gz">Meta 12 cohorts Plasma Pooled</option>
+<option class="level2" value="pQTL/10_carland_2023/sig_pairs/10_carland_2023_male.txt.gz">Meta 12 cohorts Plasma Male</option>
+<option class="level2" value="pQTL/10_carland_2023/sig_pairs/10_carland_2023_female.txt.gz">Meta 12 cohorts Plasma Female</option>
 
 <option class="level1" value="null"> 11_niu_2025 (5) </option>
-<option class="level2" value="pQTL/11_niu_2025/11_niu_2025_discovery_relaxedP.txt.gz">HOLBAEK Plasma Discovery Relaxed P</option>
-<option class="level2" value="pQTL/11_niu_2025/11_niu_2025_discovery_stringentP.txt.gz">HOLBAEK Plasma Discovery Stringent P</option>
-<option class="level2" value="pQTL/11_niu_2025/11_niu_2025_independent.txt.gz">HOLBAEK Plasma Independent</option>
-<option class="level2" value="pQTL/11_niu_2025/11_niu_2025_replicatedAdult.txt.gz">HOLBAEK Plasma Replicated Adult</option>
-<option class="level2" value="pQTL/11_niu_2025/11_niu_2025_replicatedChildren.txt.gz">HOLBAEK Plasma Replicated Children</option>
+<option class="level2" value="pQTL/11_niu_2025/sig_pairs/11_niu_2025_discovery_relaxedP.txt.gz">HOLBAEK Plasma Discovery Relaxed P</option>
+<option class="level2" value="pQTL/11_niu_2025/sig_pairs/11_niu_2025_discovery_stringentP.txt.gz">HOLBAEK Plasma Discovery Stringent P</option>
+<option class="level2" value="pQTL/11_niu_2025/sig_pairs/11_niu_2025_independent.txt.gz">HOLBAEK Plasma Independent</option>
+<option class="level2" value="pQTL/11_niu_2025/sig_pairs/11_niu_2025_replicatedAdult.txt.gz">HOLBAEK Plasma Replicated Adult</option>
+<option class="level2" value="pQTL/11_niu_2025/sig_pairs/11_niu_2025_replicatedChildren.txt.gz">HOLBAEK Plasma Replicated Children</option>
 
 <option class="level1" value="null"> 12_yang_2021 (1) </option>
-<option class="level2" value="pQTL/12_yang_2021/12_yang_2021_plasma.txt.gz">Multi tisssues Plasma</option>
+<option class="level2" value="pQTL/12_yang_2021/sig_pairs/12_yang_2021_plasma.txt.gz">Multi tisssues Plasma</option>
