b68cf34f13f6002a733ed3a76123c49d07343189
tanyaphung <tanyaphung>
2026-04-30 18:10:12 +0200

    update qtl mapping

--- resources/views/snp2gene/xqtls_options/pqtls/_pqtl_brain_options.blade.php
+++ resources/views/snp2gene/xqtls_options/pqtls/_pqtl_brain_options.blade.php
@@ -1,2 +1,2 @@
 <option class="level1" value="null"> 12_yang_2021 (1) </option>
-<option class="level2" value="pQTL/12_yang_2021/sig_pairs/12_yang_2021_brain.txt.gz">Multi tisssues Brain</option>
+<option class="level2" value="pQTL/12_yang_2021/sig_pairs/Brain_12_yang_2021.txt.gz">Multi tisssues Brain</option>
