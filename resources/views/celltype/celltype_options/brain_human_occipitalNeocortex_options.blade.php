1de0a16ebe529ce872f444d7be8d0656f8e8fe42
tanyaphung <tanyaphung>
2026-06-01 11:32:04 +0200

    fix_typo

--- resources/views/celltype/celltype_options/brain_human_occipitalNeocortex_options.blade.php
+++ resources/views/celltype/celltype_options/brain_human_occipitalNeocortex_options.blade.php
@@ -17,7 +17,7 @@ data-section="Brain/Human/Occipital Neocortex/Postnatal/Level1" data-key="3">
 
 <!-- postnatal level 2 -->
 <option value="29_Siletti_CerebralCortex.V2_Human_2022_level2"
-data-section="Brain/Human/Occipital Neocortex/Postnatal/Level2" data-key="0>
+data-section="Brain/Human/Occipital Neocortex/Postnatal/Level2" data-key="0">
 29_Siletti_CerebralCortex.V2_Human_2022_level2</option>
 
 <option value="36_Siletti_CerebralCortex.Pro_Human_2022_level2"
